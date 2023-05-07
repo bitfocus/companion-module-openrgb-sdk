fe77d261a5329e4801a2aa40c2d08fedc1930434
Julian Waller <git@julusian.co.uk>
2023-05-07 20:12:32 +0100

    wip: prototype

--- actions.js
+++ actions.js
@@ -1,19 +1,126 @@
-module.exports = function (self) {
+import { combineRgb, splitRgb } from '@companion-module/base'
+
+function parseColor(color) {
+	const raw = splitRgb(color)
+
+	return {
+		red: raw.r,
+		green: raw.g,
+		blue: raw.b,
+	}
+}
+
+export function UpdateActions(self) {
+	const devicePicker = {
+		id: 'deviceIds',
+		type: 'multidropdown',
+		label: 'Devices',
+		default: [],
+		choices: [],
+	}
+
+	for (const [deviceId, device] of Object.entries(self.devices)) {
+		devicePicker.choices.push({
+			id: deviceId,
+			label: device.name,
+		})
+	}
+	console.log(devicePicker.choices, self.devices)
+
 	self.setActionDefinitions({
-		sample_action: {
-			name: 'My First Action',
+		updateLeds: {
+			name: 'Set All LEDs',
 			options: [
+				devicePicker,
 				{
-					id: 'num',
-					type: 'number',
-					label: 'Test',
-					default: 5,
-					min: 0,
-					max: 100,
+					id: 'color',
+					type: 'colorpicker',
+					label: 'Color',
+					default: combineRgb(255, 255, 255),
 				},
 			],
 			callback: async (event) => {
-				console.log('Hello world!', event.options.num)
+				const color = parseColor(event.options.color)
+
+				const updates = []
+				for (const id of event.options.deviceIds) {
+					const device = self.devices[id]
+					if (device) {
+						const colors = []
+						for (let i = 0; i < device.leds.length; i++) {
+							colors.push(color)
+						}
+						updates.push(self.client.updateLeds(device.deviceIndex, colors))
+					}
+				}
+
+				await Promise.allSettled(updates)
+			},
+		},
+		setSingleLed: {
+			name: 'Set Single LED',
+			options: [
+				devicePicker,
+				{
+					id: 'ledIndex',
+					type: 'number',
+					label: 'LED Index',
+					min: 0,
+					default: 0,
+				},
+				{
+					id: 'color',
+					type: 'colorpicker',
+					label: 'Color',
+					default: combineRgb(255, 255, 255),
+				},
+			],
+			callback: async (event) => {
+				const color = parseColor(event.options.color)
+
+				const updates = []
+				for (const id of event.options.deviceIds) {
+					const device = self.devices[id]
+					if (device) {
+						updates.push(self.client.updateSingleLed(device.deviceIndex, event.options.ledIndex, color))
+					}
+				}
+
+				await Promise.allSettled(updates)
+			},
+		},
+		setSingleLedByName: {
+			name: 'Set Single LED',
+			options: [
+				devicePicker,
+				{
+					id: 'ledName',
+					type: 'textinput',
+					label: 'LED Name',
+					default: '',
+				},
+				{
+					id: 'color',
+					type: 'colorpicker',
+					label: 'Color',
+					default: combineRgb(255, 255, 255),
+				},
+			],
+			callback: async (event) => {
+				const color = parseColor(event.options.color)
+
+				const updates = []
+				for (const id of event.options.deviceIds) {
+					const device = self.devices[id]
+					if (device) {
+						const ledIndex = device.leds.findIndex((l) => l.name == event.options.ledName)
+						if (ledIndex !== -1) {
+							updates.push(self.client.updateSingleLed(device.deviceIndex, ledIndex, color))
+						}
+					}
+				}
+
+				await Promise.allSettled(updates)
 			},
 		},
 	})
--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-const { InstanceBase, Regex, runEntrypoint, InstanceStatus } = require('@companion-module/base')
-const UpgradeScripts = require('./upgrades')
-const UpdateActions = require('./actions')
-const UpdateFeedbacks = require('./feedbacks')
-const UpdateVariableDefinitions = require('./variables')
+import { Client } from 'openrgb-sdk'
+import { isEqual } from 'lodash-es'
+import { InstanceBase, Regex, runEntrypoint, InstanceStatus } from '@companion-module/base'
+import { UpgradeScripts } from './upgrades.js'
+import { UpdateActions } from './actions.js'
+import { UpdateFeedbacks } from './feedbacks.js'
+import { UpdateVariableDefinitions } from './variables.js'
 
-class ModuleInstance extends InstanceBase {
+class OpenRGBInstance extends InstanceBase {
 	constructor(internal) {
 		super(internal)
+
+		this.devices = {}
 	}
 
 	async init(config) {
-		this.config = config
+		await this.configUpdated(config)
 
-		this.updateStatus(InstanceStatus.Ok)
-
+		this.devices = {}
 		this.updateActions() // export actions
 		this.updateFeedbacks() // export feedbacks
 		this.updateVariableDefinitions() // export variable definitions
@@ -21,10 +24,64 @@ class ModuleInstance extends InstanceBase {
 	// When module gets deleted
 	async destroy() {
 		this.log('debug', 'destroy')
+
+		if (this.client) {
+			await this.client.disconnect().catch((e) => {
+				// Ignore
+			})
+			delete this.client
+		}
 	}
 
 	async configUpdated(config) {
+		const oldConfig = this.config || {}
 		this.config = config
+
+		if (this.client) {
+			this.startStopPolling(false)
+			this.updateStatus(InstanceStatus.Disconnected)
+
+			this.client.disconnect().catch((e) => {
+				// Ignore
+			})
+			delete this.client
+		}
+
+		if (this.config.port !== oldConfig.port || this.config.host !== oldConfig.host) {
+			// Reset any tracked state
+			this.devices = {}
+			this.updateActions() // export actions
+			this.updateFeedbacks() // export feedbacks
+			this.updateVariableDefinitions() // export variable definitions
+
+			this.client = new Client('Companion', this.config.port, this.config.host)
+			this.updateStatus(InstanceStatus.Connecting)
+
+			this.client.on('connect', () => this.startStopPolling(true))
+			this.client.on('disconnect', () => this.startStopPolling(true))
+			this.client.on('deviceListUpdated', () =>
+				this.doPollDevices().catch((e) => {
+					this.log('error', `Poll failed: ${e?.message ?? e}`)
+				})
+			)
+
+			this.client
+				.connect()
+				.then(async () => {
+					this.updateStatus(InstanceStatus.Ok)
+					this.startStopPolling(true)
+
+					this.doPollDevices()
+				})
+				.catch((e) => {
+					this.updateStatus(InstanceStatus.UnknownError, `Connection failed: ${e?.message ?? e}`)
+				})
+		}
+
+		if (this.config.poll_interval !== oldConfig.poll_interval && this.pollTimer) {
+			this.startStopPolling(false)
+			this.startStopPolling(true)
+		}
 	}
 
 	// Return config fields for web config
@@ -36,15 +93,75 @@ class ModuleInstance extends InstanceBase {
 				label: 'Target IP',
 				width: 8,
 				regex: Regex.IP,
+				default: '127.0.0.1',
 			},
 			{
-				type: 'textinput',
+				type: 'number',
 				id: 'port',
 				label: 'Target Port',
 				width: 4,
-				regex: Regex.PORT,
+				min: 1,
+				max: 65535,
+				default: 6742,
+			},
+			{
+				type: 'number',
+				id: 'poll_interval',
+				label: 'Poll Interval',
+				width: 4,
+				min: 0,
+				max: 600000,
+				default: 30000,
 			},
 		]
+	}
+
+	startStopPolling(run) {
+		if (this.pollTimer && !run) {
+			clearInterval(this.pollTimer)
+			delete this.pollTimer
+		}
+
+		if (run && !this.pollTimer && this.config.poll_interval) {
+			this.pollTimer = setInterval(() => {
+				this.doPollDevices().catch((e) => {
+					this.log('error', `Poll failed: ${e?.message ?? e}`)
+				})
+			}, this.config.poll_interval)
+		}
+	}
+
+	async doPollDevices() {
+		// TODO - ensure not run concurrently
+
+		// TODO - listen to deviceListUpdated instead of timed polling?
+
+		const newDevices = {}
+
+		const rawDevices = await Promise.all(await this.client.getAllControllerData())
+		for (const device of rawDevices) {
+			// Generate an id that should be stable, unlike the `deviceId` used by the api which is an index
+			const syntheticId = `${device.name || device.vendor}:${device.serial || device.location}`
+
+			newDevices[syntheticId] = {
+				deviceIndex: device.deviceId,
+				name: device.name,
+				leds: (device.leds || []).map((l) => l.name),
+			}
+			// console.log('dev', device)
+		}
+
+		if (!isEqual(this.devices, newDevices)) {
+			this.devices = newDevices
+			console.log('devices', newDevices, JSON.stringify(rawDevices, undefined, 4))
+
+			// TODO - performance...
+			this.updateActions()
+			this.updateFeedbacks()
+
+			// TODO - update variables
+			this.checkFeedbacks()
+		}
 	}
 
 	updateActions() {
@@ -60,4 +177,4 @@ class ModuleInstance extends InstanceBase {
 	}
 }
 
-runEntrypoint(ModuleInstance, UpgradeScripts)
+runEntrypoint(OpenRGBInstance, UpgradeScripts)
